320d418cec2f01ef81a76c37981848f94aadf59d
gzhegow <6562680@gmail.com>
2025-01-10 19:40:11 +0300

    main

--- src/Package/Illuminate/Database/Schema/EloquentSchemaBlueprint.php
+++ src/Package/Illuminate/Database/Schema/EloquentSchemaBlueprint.php
@@ -2,7 +2,9 @@
 
 namespace Gzhegow\Database\Package\Illuminate\Database\Schema;
 
+use Gzhegow\Lib\Lib;
 use Gzhegow\Database\Core\OrmFactoryInterface;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
 use Illuminate\Database\Schema\Blueprint as EloquentSchemaBlueprintBase;
 
 
@@ -23,5 +25,21 @@ class EloquentSchemaBlueprint extends EloquentSchemaBlueprintBase
         $this->factory = $factory;
 
         parent::__construct($table, $callback, $prefix);
+    }
+
+
+    protected function createIndexName($type, array $columns) : string
+    {
+        /** @see parent::createIndexName() */
+
+        $theStr = Lib::str();
+
+        $table = "{$this->prefix}{$this->table}";
+
+        $tableCut = implode('_', array_map([ $theStr, 'prefix' ], explode('_', $table)));
+        $typeCut = $theStr->prefix($type);
+        $columnsCut = crc32(serialize($columns));
+
+        return "{$tableCut}_{$typeCut}{$columnsCut}";
     }
 }
